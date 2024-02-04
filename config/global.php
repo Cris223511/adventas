18a85445f7db38615f0db58c1187c8486ac81f2c
cris223511 <cris_antonio2001@hotmail.com>
2024-02-03 20:31:25 -0500

    [Christopher] cambio a base de datos principal

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "adventas2");
+define("DB_NAME", "adventas");
 
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
