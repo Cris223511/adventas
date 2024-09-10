c738faa1b0bd3153cdb6391a0e1d6df176975876
cris223511 <cris_antonio2001@hotmail.com>
2024-09-10 04:31:06 -0500

    [Christopher] corrijo labeel de nombre empresa

--- vistas/devoluciones.php
+++ vistas/devoluciones.php
@@ -115,7 +115,7 @@ if (!isset($_SESSION["nombre"])) {
                   <input type="number" class="form-control" name="telefono" id="telefono3" maxlength="9" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                  <label class="label_serie">Nombre o empresa(*):</label>
+                  <label class="label_serie">Nombre o empresa:</label>
                   <input type="text" class="form-control" name="empresa" id="empresa3" maxlength="50" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-12 col-md-12">
@@ -191,7 +191,7 @@ if (!isset($_SESSION["nombre"])) {
                   <input type="number" class="form-control" name="telefono" id="telefono2" maxlength="9" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                  <label class="label_serie">Nombre o empresa(*):</label>
+                  <label class="label_serie">Nombre o empresa:</label>
                   <input type="text" class="form-control" name="empresa" id="empresa2" maxlength="50" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-12 col-md-12">
@@ -286,7 +286,7 @@ if (!isset($_SESSION["nombre"])) {
                   <input type="number" class="form-control" name="telefono" id="telefono" maxlength="9" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                  <label class="label_serie">Nombre o empresa(*):</label>
+                  <label class="label_serie">Nombre o empresa:</label>
                   <input type="text" class="form-control" name="empresa" id="empresa" maxlength="50" placeholder="Sin registrar." disabled>
                 </div>
                 <div class="form-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
